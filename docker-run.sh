3c4f93113fcbbb9866cd800e40ca1b09c78088e7
huawei laptop <huawei@laptop.com>
2021-01-27 18:43:06 +0800

    docker build and up

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 
-git clone https://github.com/smallsaas/sandbox-api.git Docker
+root=${PWD}
+echo root=$root
 
-cd Docker
-docker-compose up
+mkdir -p $root/Docker
+# docker-compose.yml
+echo "curl -sL https://../docker-compose.yml -o $root/Docker/docker-compose.yml"
+curl -sL https://raw.githubusercontent.com/smallsaas/sandbox/master/docker-compose.yml -o $root/Docker/docker-compose.yml
+if [ ! -f $root/Docker/docker-compose.yml ];then
+  echo download docker-compose.yml fails !
+  exit
+fi
+
+## config
+mkdir -p  $root/config
+echo "curl -sL https://../config/application.yml -o $root/config/application.yml"
+curl -sL https://raw.githubusercontent.com/smallsaas/sandbox/master/config/application.yml -o $root/config/application.yml
+if [ ! -f $root/config/application.yml ];then
+  echo download application.yml fails !
+  exit
+fi
+
+## run
+cd $root
+docker-compose run --rm api
