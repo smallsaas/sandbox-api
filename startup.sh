d3521b9bf10ef2d031d1e8a8dc08f608b2f917db
Vincent <a30f49@gmail.com>
2021-05-08 13:46:12 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # COMPOSE_DOCKER_CLI_BUILD=1 docker-compose up --build --remove-orphans
-COMPOSE_DOCKER_CLI_BUILD=1 docker-compose up --build
+COMPOSE_DOCKER_CLI_BUILD=1 docker-compose up --build --detach
  
